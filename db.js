f516a36511b94d810a1bc09750f6f9089869cce1
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-04-25 18:14:48 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,6 +3,6 @@ import mysql from "mysql"
 export const db = mysql.createConnection({
     host: "bbkhxcrmbqp79ewu78no-mysql.services.clever-cloud.com",
     user: "umskwtz3svdbyzec",
-    password: "umskwtz3svdbyzec",
+    password: "7z11I9bM4d1ObpkItYsp",
     database: "bbkhxcrmbqp79ewu78no"
 })
